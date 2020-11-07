48309f9ff065ee44c2285a6b0d6be4e3d92f8fdd
kokkiemouse <kokkiemouse@gmail.com>
2020-11-07 15:49:49 +0900

    fixed typo
    Signed-off-by: kokkiemouse <kokkiemouse@gmail.com>

--- build_f33.sh
+++ build_f33.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 docker build -t serene-mockbuild:latest .
-docker run --cap-add=SYS_ADMIN -t -i serene-mockbuild:latest bash -c cd /home/mockbuild/serenelinux-rpmbuilds\;git pull -f;./build_srpms.py\;./build_docker_srpm_to_rpm.py fedora-33-x86_64
+docker run --cap-add=SYS_ADMIN -t -i serene-mockbuild:latest bash -c cd /home/mockbuild/serenelinux-rpmbuilds\;git pull -f\;./build_srpms.py\;./build_docker_srpm_to_rpm.py fedora-33-x86_64
