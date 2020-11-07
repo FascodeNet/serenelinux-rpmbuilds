884605b96b68972a6db23cb9f17bb7de7d4570c4
kokkiemouse <kokkiemouse@gmail.com>
2020-11-07 15:47:52 +0900

    fixed
    Signed-off-by: kokkiemouse <kokkiemouse@gmail.com>

--- build_f33.sh
+++ build_f33.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-docker build --no-cache=true -t serene-mockbuild:latest .
+docker build -t serene-mockbuild:latest .
 docker run --cap-add=SYS_ADMIN -t -i serene-mockbuild:latest "bash -c cd /home/mockbuild/serenelinux-rpmbuilds;git pull -f;./build_srpms.py;./build_docker_srpm_to_rpm.py fedora-33-x86_64"
