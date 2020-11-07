03b861f98f951da4215d95ab3bb04d89f1e4dc99
kokkiemouse <kokkiemouse@gmail.com>
2020-11-07 10:05:12 +0900

    added srpms script
    Signed-off-by: kokkiemouse <kokkiemouse@gmail.com>

---
+++ build_srpms.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+import subprocess
+import pathlib
+import os
+import shutil
+import sys
+import glob
+#def gen_srpm(specfile):
+#    subprocess.call(["spectool", "-g",specfile])
+def gen_srpm():
+    spec_files =  glob.glob("./**/*.spec")
+    for spec_f in spec_files:
+        subprocess.call(["spectool", "-g",spec_f])
+        subprocess.call(["rpmbuild","-bs",spec_f])
+def main():
+    gen_srpm()
+    
+if __name__ == '__main__':
+    main()
